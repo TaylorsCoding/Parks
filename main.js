8095cf0248163902095db40b1e48d8573e4ed563
codingbeginner19 <60525942+codingbeginner19@users.noreply.github.com>
2020-06-04 21:53:44 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,18 +3,21 @@ $(function () {
         event.preventDefault();
         let states = $("input[name=state]").val();
         let numberOfParks = $("input[name=park]").val();
-        let stateArray = states.split(" ");
         let limit = numberOfParks || 10;
-        let url = makeUrl(stateArray, limit)
+        let url = makeUrl(states, limit)
 
         fetch(url)
             .then(response => {
+                console.log(response);
                 if (response.ok) {
                     return response.json();
                 }
                 throw new Error(response.statusText);
             })
-            .then(responseJson => console.log(JSON.stringify(responseJson)))
+            .then(responseJson => {
+                let parks = responseJson.data;
+                displayParks(parks)
+            })
             .catch(err => {
                 $('#js-error-message').text(`Something went wrong: ${err.message}`);
             });
@@ -23,17 +26,32 @@ $(function () {
 
 
 function makeUrl(states, number) {
-    let url = 'https://developer.nps.gov/api/v1/parks?api_key=si04k94062nZ9ws3gExiO6H17J1Ka1zbKUtJINQo';
+    let url = `https://developer.nps.gov/api/v1/parks?stateCode=${states}&api_key=si04k94062nZ9ws3gExiO6H17J1Ka1zbKUtJINQo&limit=${number}`;
 
-    for (let i = 0; i < states.length; i++) {
-        url += `&stateCode=${states[i]}`;
-    }
-
-    url += `&limit=${number}`
     return url;
 }
 
-
-
-//how do you turn a string into an array?
-// "va pa ca"
+function displayParks(parks) {
+    $('#parksTable').remove();
+    const table = "<table id='parksTable'> \
+                        <thead> \
+                            <tr> \
+                                <th>State</th> \
+                                <th>Name</th> \
+                                <th>Description</th> \
+                                <th>Website</th> \
+                            </tr> \
+                        </thead> \
+                        <tbody> \
+                        </tbody> \
+                    </table> "
+    $('#parksContainer').append(table);
+    $.each(parks, function (index, park) {
+        let info = park.description
+        let parkName = park.fullName
+        let webUrl = "http://www.nps.gov/" + park.parkCode
+        let states = park.states
+        console.log(`<tr><td>${states}</td><td>${parkName}</td><td>${info}</td><td>${webUrl}</td></tr>`)
+        $('#parksTable tbody').append(`<tr><td>${states}</td><td>${parkName}</td><td>${info}</td><td><a href="${webUrl}">${webUrl}</a></td></tr>`);
+    })
+}
